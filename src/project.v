385f069109cf0367d51c9d9e2c82f049275d3b97
Syed <alam.syedmz@gmail.com>
2025-06-20 07:37:34 -0400

    fixed top module name

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_wasic_onboarding_syed (
+module tt_um_uwasic_onboarding_syed (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
