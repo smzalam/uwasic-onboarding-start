ee9c49651fe3c8d0ac56c8dd7d3f5efcebdc1104
Syed <alam.syedmz@gmail.com>
2025-09-12 21:52:09 -0400

    fixed undriven wires

--- src/project.v
+++ src/project.v
@@ -48,11 +48,6 @@ module tt_um_uwasic_onboarding_syed (
     .out({uio_out, uo_out})
   );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, ui_in[7:3], uio_in, 1'b0};
 
